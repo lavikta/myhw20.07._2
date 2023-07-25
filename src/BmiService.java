65a65ca1a49e9060f0ddea601588402f3a926678
Victoria Karetnikova <karetnikova.test@gmail.com>
2023-07-25 10:51:32 -0400

    Formula changed

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
+
     public int calculate(double height, int weight) {
-        double bmIndex = height * 100 / weight;
-        return bmIndex;
+        return (int) (weight / height /height);
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        BmiService bmiService = new BmiService();
-        double height = 1.87; // рост
-        int weight = 98; // вес
-        int bmiService = bmiService.calculate(height, weight);
-    }
 
+        BmiService bmiService = new BmiService();
+        double height = 1.63; // рост
+        int weight = 63; // вес
+
+        int bmiIndex = bmiService.calculate(height, weight);
+        System.out.println("Body mass index: " + bmiIndex);
+    }
 }
